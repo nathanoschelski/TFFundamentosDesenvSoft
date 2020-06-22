a2dbf35a1de5e018131bbf3c85620addf77e3e6c
p-karol <priscilla.karolczak@gmail.com>
2020-06-22 22:07:34 +0100

    Adicionando estrutura de classes do sistema

---
+++ sales/src/main/java/com/sales/casosdeuso/Fachada.java
@@ -0,0 +1,13 @@
+package main.java.com.sales.entidades;
+
+public class Fachada {
+
+    public Produto criarProduto(double salesPrice, double transferPrice, String nome){
+        Produto produto = new Produto(costPrice, transferPrice, nome);
+        return Produto;
+    }
+
+
+
+}
+
---
+++ sales/src/main/java/com/sales/casosdeuso/GereVenda.java
@@ -0,0 +1,18 @@
+
+import main.java.com.sales.casosdeuso;
+
+public class GereVenda {
+
+    private VendaDTO venda;
+
+    public GereVenda(VendaDTO venda) {
+        this.venda = venda;
+    }
+
+    public double calcularSalesPrice(Produto produto, double margem){
+
+    }
+
+    
+
+}
