8fe9b6440d3cfc65c0dde17c63ea439dbd537655
p-karol <priscilla.karolczak@gmail.com>
2020-06-22 10:56:43 +0100

    adicionada caso de uso VendaDTO

---
+++ sales/src/main/java/com/sales/casosdeuso/VendaDTO.java
@@ -0,0 +1,49 @@
+import main.java.com.sales.casosdeuso;
+
+public class VendaDTO {
+
+    private Produto produto;
+    private double margem;
+    private double salesPrice;
+
+    public VendaDTO(double margem, double salesPrice, Produto produto) {
+        this.margem = margem;
+        this.salesPrice = salesPrice;
+        this.produto = produto;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public double getMargem() {
+        return margem;
+    }
+
+    public void setMargem(double margem) {
+        this.margem = margem;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public String toString() {
+        return "VendaDTO [produto=" + produto.getNome() + ", margem=" + margem + ", salesPrice=" + salesPrice + "]";
+    }
+
+    
+
+
+    
+
+
+}
