2ccbab8a54cf533b7e84b2477d3e52b7628ce7f0
p-karol <priscilla.karolczak@gmail.com>
2020-07-04 15:34:53 +0100

    spring

--- sales/src/main/java/com/sales/interfaces/SalesApplication.java
+++ sales/src/main/java/com/sales/interfaces/SalesApplication.java
@@ -1,9 +1,11 @@
-package com.sales;
+package com.sales.interfaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sales"})
+
 public class SalesApplication {
 
 	public static void main(String[] args) {
